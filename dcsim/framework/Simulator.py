f316e510f98e09fbf00d20d4af8b18315b40aacc
BreakVoid <breakvoid@hotmail.com>
2017-07-06 10:41:18 -0400

    remove property label of ConfigurationBase implement ConfigurationBase to Configuration

--- dcsim/framework/Simulator.py
+++ dcsim/framework/Simulator.py
@@ -5,7 +5,10 @@ if TYPE_CHECKING:
 
 class Simulator:
     def __init__(self, config: Type['ConfigurationBase']) -> None:
-        raise NotImplementedError
+        self.network_controller = config.get_network_controller_type()()
+        self.adversary_controller = config.get_adversary_controller_type()()
+        self.honest_nodes = [config.get_honest_node_type()() for _ in range(0, config.get_num_nodes)]
+        
 
     def run(self):
         raise NotImplementedError
